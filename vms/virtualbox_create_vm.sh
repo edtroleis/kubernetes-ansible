04dbbba841a33f38e84ccda445466b8b004882cb
edtroleis <edtroleis@outlook.com>
2022-08-22 01:48:26 -0300

    File to create vms

--- vms/virtualbox_create_vm.sh
+++ vms/virtualbox_create_vm.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 MACHINENAME=$1
 
-DISK_FILENAME="ubuntu-22.04-64bit.7z"
+GDRIVE_FILEID="1407kJDebHgxqAN0du0_jhH6kZDBXW5q6"
+GDRIVE_FILENAME="ubuntu-22.04-64bit.tar.gz"
+
+BASE_IMAGE_DIR="/mnt/e/virtualbox_baseimage"
+BASE_IMAGE_NAME="ubuntu-22.04-64bit.vdi"
+
+VM_BASE_DIR_LNX="/mnt/e/VirtualBoxVMs"
 DISK_VDI=${MACHINENAME}.vdi
-BASE_FOLDER="E:\VirtualBoxVMs"
+VM_BASE_DIR_WIN="E:\VirtualBoxVMs"
 
-# https://doc-0g-7g-docs.googleusercontent.com/docs/securesc/04h2clh4ceohtt2c4o7bv1eoepn7dlfj/5kllp5332c6ag075csdhl14h1r77mkel/1660537350000/03854773277332866080/03035033603652941582/1407kJDebHgxqAN0du0_jhH6kZDBXW5q6?e=download&ax=AI9vYm4ZqmZR5tynDNwpZJ8g_EhoCvbqV8aJdSILrwuDKDv5e0eGiUMY6G5JDzpmx6m3QQo4XgFD0bomkygiaJI46Okum1BoqS-LFJZlYc6NbnJIPSRXbfY5-d5GcQeT9NHOSYV_C-lfVVOTDA2V3Gr-I6X2dW7M1opGi7VN8anGzw9yXIKp4NhkUk2aGjn3k79QBYRezr69EwgRcfpYlVJqDjQQpWdHkn1UahfroVh_lwYbU_ufwbDHaEurWv0z6r1jM2Znc4OHGSLIS2DtjH53HGP4dlmAqpRQc7Q4d-U_1wSCHsNKqb8cnXFU9OdfkxMs4EEg0VfmcmlKIAQ1ZgqD8XUS5A7gtNDmA-ykOZP5xbr1MFx1XqdCxYElGyPB1dQy27Ma6avmNQCUALd__IW0PrQsFIOZ3txZX1HcXWch_cBiOmmRE0pYn7vyeFrruJk7hbjK7llz6sZFc6a1gXCD4BuBbPOmpmCMFOFEokQD2zmDTcydknjr9LtQncbKKfeQq1t7KCq9Rk64oUIHQB0ydeINHx7F8UB9x3aUr7WQxLgQ7aTrqRXmvA05uMS_BzIWBXbHRK5mEmk4gXZXHF2U93gqiEcatmO6Q8jaVOcM6OsQusQO_6hgfW8zk4JHc7s-lk6o45KmdHrJ7hL87r9LUEcCQz3RjjC_6mEknSaaGZKjG5BCfetJ_gdgm2smMp2O-MmE5tBqPnlC3Gpq&uuid=27165e3f-3393-42ba-86a4-8eb8f2bbe71e&authuser=0
+if [ ! -d $BASE_IMAGE_DIR ] 
+then
+    echo "$BASE_IMAGE_DIR does not exist, creating..." 
+    mkdir -p $BASE_IMAGE_DIR
+fi
 
-# Download disk vdi
-# if [ ! -f ./$DISK_FILENAME ]; then
-#     wget https://ufpr.dl.sourceforge.net/project/osboxes/v/vb/55-U-u/22.04/64bit.7z -O $DISK_FILENAME | 7z x $DISK_FILENAME
-# fi
+# Download disk from Google Drive
+if [ ! -f $BASE_IMAGE_DIR/$BASE_IMAGE_NAME ]; then
+  html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${GDRIVE_FILEID}"`
+  curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${GDRIVE_FILEID}" -o $BASE_IMAGE_DIR/${GDRIVE_FILENAME}
+  tar -zxvf $BASE_IMAGE_DIR/${GDRIVE_FILENAME} -C $BASE_IMAGE_DIR
+fi
 
 #Create VM
-VBoxManage.exe createvm --name $MACHINENAME --ostype "Ubuntu_64" --register --basefolder $BASE_FOLDER
+VBoxManage.exe createvm --name $MACHINENAME --ostype "Ubuntu_64" --register --basefolder $VM_BASE_DIR_WIN
 
 # Create disk and connect
 # Chage uuid disk to create vms using images from osboxes
-if [ ! -f /mnt/e/VirtualBoxVMs/$MACHINENAME/$DISK_VDI ]; then
-  # VBoxManage.exe internalcommands sethduuid ./64bit/'Ubuntu 22.04 (64bit).vdi'
-  cp /mnt/e/virtualbox_baseimage/ubuntu-22.04-64bit.vdi /mnt/e/VirtualBoxVMs/$MACHINENAME/$DISK_VDI
-  VBoxManage.exe internalcommands sethduuid 'E:/virtualbox_baseimage/ubuntu-22.04-64bit.vdi'
+if [ ! -f $VM_BASE_DIR_LNX_LNX/$vm/$DISK_VDI ]; then
+  cp $BASE_IMAGE_DIR/$BASE_IMAGE_NAME $VM_BASE_DIR_LNX_LNX/$vm/$DISK_VDI
+  VBoxManage.exe internalcommands sethduuid "$VM_BASE_DIR_LNX_WIN\\$vm\\$DISK_VDI"
 fi
 
 # Connect disk
 VBoxManage.exe storagectl $MACHINENAME --name "SATA Controller" --add sata --controller IntelAhci
-VBoxManage.exe storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "E:\VirtualBoxVMs\\$MACHINENAME\\$DISK_VDI"
+VBoxManage.exe storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VM_BASE_DIR_WIN\\$vm\\$DISK_VDI"
 
 # Geral
 VBoxManage.exe modifyvm $MACHINENAME --draganddrop bidirectional \
---
+++ vms/virtualbox_create_vms_k8s.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+GDRIVE_FILEID="1407kJDebHgxqAN0du0_jhH6kZDBXW5q6"
+GDRIVE_FILENAME="ubuntu-22.04-64bit.tar.gz"
+
+BASE_IMAGE_DIR="/mnt/e/virtualbox_baseimage"
+BASE_IMAGE_NAME="ubuntu-22.04-64bit.vdi"
+
+VM_BASE_DIR_LNX="/mnt/e/VirtualBoxVMs"
+
+VM_BASE_DIR_WIN="E:\VirtualBoxVMs"
+
+VMS=('k-master' 'k-worker1' 'k-worker2')
+
+if [ ! -d $BASE_IMAGE_DIR ] 
+then
+    echo "$BASE_IMAGE_DIR does not exist, creating..." 
+    mkdir -p $BASE_IMAGE_DIR
+fi
+
+# Download disk from Google Drive
+if [ ! -f $BASE_IMAGE_DIR/$BASE_IMAGE_NAME ]; then
+  html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${GDRIVE_FILEID}"`
+  curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${GDRIVE_FILEID}" -o $BASE_IMAGE_DIR/${GDRIVE_FILENAME}
+  tar -zxvf $BASE_IMAGE_DIR/${GDRIVE_FILENAME} -C $BASE_IMAGE_DIR
+fi
+
+for vm in "${VMS[@]}"
+do
+  printf "### Creating VM $vm...\n"
+  DISK_VDI="${vm}.vdi"
+
+  #Create VM
+  VBoxManage.exe createvm --name $vm --ostype "Ubuntu_64" --register --basefolder $VM_BASE_DIR_WIN
+
+  # Create disk and connect
+  # Chage uuid disk to create vms using images from osboxes
+  if [ ! -f $VM_BASE_DIR_LNX/$vm/$DISK_VDI ]; then
+    cp $BASE_IMAGE_DIR/$BASE_IMAGE_NAME $VM_BASE_DIR_LNX/$vm/$DISK_VDI
+    VBoxManage.exe internalcommands sethduuid "$VM_BASE_DIR_WIN\\$vm\\$DISK_VDI"
+  fi
+
+  # Connect disk
+  VBoxManage.exe storagectl $vm --name "SATA Controller" --add sata --controller IntelAhci
+  VBoxManage.exe storageattach $vm --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VM_BASE_DIR_WIN\\$vm\\$DISK_VDI"
+
+  # Geral
+  VBoxManage.exe modifyvm $vm --draganddrop bidirectional \
+                                      --clipboard-mode bidirectional
+
+  # System
+  VBoxManage.exe modifyvm $vm --ioapic on \
+                                      --memory 2048 \
+                                      --cpus 2 \
+                                      --boot1 disk --boot2 none --boot3 none \
+                                      --rtcuseutc on \
+                                      --pae off
+
+  # Monitor
+  VBoxManage.exe modifyvm $vm --vram 64 \
+                                      --graphicscontroller vmsvga \
+                                      --vrdevideochannelquality 125
+
+  # Audio
+  VBoxManage.exe modifyvm $vm --audio none
+
+  # Network
+  VBoxManage.exe modifyvm $vm --nic1 natnetwork \
+                                      --nat-network1 NatNetwork \
+                                      --nic2 hostonly \
+                                      --hostonlyadapter2 'VirtualBox Host-Only Ethernet Adapter'
+
+  # Shared folder
+  VBoxManage.exe sharedfolder add $vm --name='shared' --hostpath='E:\shared' --automount --auto-mount-point='/mnt/shared'
+
+  #Start the VM
+  VBoxManage.exe startvm $vm
+
+  printf "### VM $vm created succesfully!\n\n"
+  sleep 15
+done
+
+# Set entries on /etc/hosts
+printf "### Adding hostname/IP on /etc/hosts\n"
+sleep 15
+printf "\n# Ansible hosts\n" >> /etc/hosts
+
+for vm in "${VMS[@]}"
+do
+  VM_IP=$(VBoxManage.exe guestproperty enumerate "$vm" | grep "/VirtualBox/GuestInfo/Net/1/V4/IP" | grep -o -w -P -e '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+  printf "$VM_IP\t$vm\n" >> /etc/hosts
+done
+printf "### Hostname/IP added on /etc/hosts\n"
